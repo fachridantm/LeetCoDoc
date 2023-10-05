a12be2f7816d70f0991d3dde05ff64feafc122b4
Fachridan Tio Mu'afa <fachridantm@gmail.com>
2023-10-06 00:57:05 +0700

    Time: 191 ms (85.93%), Space: 38.3 MB (46.84%) - LeetHub

--- 0001-two-sum/0001-two-sum.kt
+++ 0001-two-sum/0001-two-sum.kt
@@ -1,13 +1,13 @@
 class Solution {
     fun twoSum(nums: IntArray, target: Int): IntArray {
-        val seen = mutableMapOf<Int, Int>()
+       val seen = mutableMapOf<Int, Int>()
         for (i in nums.indices) {
             val complement = target - nums[i]
             if (seen.containsKey(complement)) {
-                return intArrayOf(seen[complement]!!, i)
+                return intArrayOf(i, seen[complement]!!)
             }
             seen[nums[i]] = i
-        }   
+        }
         return intArrayOf()
     }
 }
